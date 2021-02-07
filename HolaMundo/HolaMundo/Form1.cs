86bab7dee204c9910dc18eb76783d8b45868054b
Karen Paz <mabelpazf@gmail.com>
2021-02-07 12:59:56 -0600

    Cmbiando frase

--- HolaMundo/HolaMundo/Form1.cs
+++ HolaMundo/HolaMundo/Form1.cs
@@ -19,7 +19,7 @@ namespace HolaMundo
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hola Mundo");
+            MessageBox.Show("La vida te pondrá obstáculos pero los límites los pones tú ");
          }
     }
 }
